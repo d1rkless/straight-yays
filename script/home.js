0689223cb174c78192fae1b71f989c19afcde9ec
d1rkless <d1rklesslol@gmail.com>
2025-11-20 10:50:50 -0500

    Update home.js

--- script/home.js
+++ script/home.js
@@ -1,1 +1,12 @@
+function checkDailyGems() {
+    const today = new Date().toDateString();
+    if(user.lastDaily !== today){
+        user.gems += 20;
+        if(user.gems>50) user.gems = 50;
+        user.lastDaily = today;
+        localStorage.setItem('syUser', JSON.stringify(user));
+        updateGemDisplay();
+    }
+}
 
+if(user) checkDailyGems();
